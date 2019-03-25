73625007544847eafe1395b7889d533c396993b5
Andrew Rynhard <andrew@andrewrynhard.com>
2019-03-24 19:08:43 -0700

    feat: upgrade Linux to v4.19.31 (#8)
    Signed-off-by: Andrew Rynhard <andrew@andrewrynhard.com>

--- core/linux-headers.sh
+++ core/linux-headers.sh
@@ -1,4 +1,4 @@
-download https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.19.23.tar.xz
+download https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.19.31.tar.xz
 
 make -C ../ INSTALL_HDR_PATH=dest headers_install
 cp -rv ../dest/include/* ${TOOLCHAIN}/include
