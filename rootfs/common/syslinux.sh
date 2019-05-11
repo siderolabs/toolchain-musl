cb457dc44d702d5602e357fbd66297b34fd144f8
Andrew Rynhard <andrew@andrewrynhard.com>
2019-05-11 08:11:23 -0700

    feat: add files required by UEFI (#25)
    Signed-off-by: Andrew Rynhard <andrew@andrewrynhard.com>

--- rootfs/common/syslinux.sh
+++ rootfs/common/syslinux.sh
@@ -11,3 +11,5 @@ make -j $(nproc) installer
 cp bios/extlinux/extlinux /rootfs/bin
 mkdir -p /rootfs/usr/lib/syslinux
 cp efi64/mbr/gptmbr.bin /rootfs/usr/lib/syslinux/
+cp efi64/efi/syslinux.efi /rootfs/usr/lib/syslinux/
+cp efi64/com32/elflink/ldlinux/ldlinux.e64 /rootfs/usr/lib/syslinux/
